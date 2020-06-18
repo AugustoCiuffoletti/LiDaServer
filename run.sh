880a0f96ccced5a4618fb7b14d3071df6afdf718
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-06-18 16:25:53 +0200

    run.sh fix

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cd
 source lidaEnv/bin/activate
 cd mlida/server
 # python app.py # development server
-gunicorn --bind 0.0.0.0:5000 app:LidaApp # production server
+gunicorn --bind 0.0.0.0:5000 lida_app:LidaApp # production server
